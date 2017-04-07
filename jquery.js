5b71abd962f71fd707ff04257e943461ba31d1c5
BrainStation <javathehut@users.noreply.github.com>
2017-04-07 17:36:52 -0400

    Styled Index, Added jQuery and Angel file

---
+++ jquery.js
@@ -0,0 +1,195 @@
+$('#davey').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#daveypic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#daveypic').css('border-radius','5px');
+
+});
+
+$('#daveypic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#davey').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#davey').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#feeras').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#feeraspic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#feeraspic').css('border-radius','5px');
+
+});
+
+$('#feeraspic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#feeras').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#feeras').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#johnny').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#johnnypic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#johnnypic').css('border-radius','5px');
+
+});
+
+$('#johnnypic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#johnny').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#johnny').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#david').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#davidpic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#davidpic').css('border-radius','5px');
+
+});
+
+$('#davidpic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#david').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#david').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#angel').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#angelpic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#angelpic').css('border-radius','5px');
+
+});
+
+$('#angelpic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#angel').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#angel').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#aida').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#aidapic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#aidapic').css('border-radius','5px');
+
+});
+
+$('#aidapic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#aida').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#aida').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
+
+$('#cathy').hover(function(){
+
+       $(this).css('font-weight','bold');
+        $('#cathypic').css('border-radius','50%');
+
+    }, function(){
+
+        $(this).css({
+            'color':'black', 
+            'font-weight':'normal'});
+        $('#cathypic').css('border-radius','5px');
+
+});
+
+$('#cathypic').hover(function(){
+
+       $(this).css('border-radius','50%');
+        $('#cathy').css('font-weight','bold');
+
+    }, function(){
+
+        $(this).css('border-radius','5px');
+        $('#cathy').css({
+            'color':'black', 
+            'font-weight':'normal'});
+
+});
